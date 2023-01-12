6111ba50bc91ede3c733b9d1737a5c86a4213faf
Rahul-Shedge <rahulshedge555@outlook.com>
2023-01-13 00:18:49 +0530

    callback utils created

---
+++ src/utils/callbacks.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+def create_and_save_tensorboard_callback(
+        callbacks_dir,
+        tensorboard_log_dir_path,
+    ):
+    pass
+
+def create_and_save_checkpoints_callback(
+        callbacks_dir,
+        checkpoint_dir,
+    ):
+    pass
+
